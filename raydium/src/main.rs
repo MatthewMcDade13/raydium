c93a0e322c8cbf6db2854ac54bcc35986b8bd65a
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2023-08-08 10:10:57 +0000

    add eframe gui

--- raydium/src/main.rs
+++ raydium/src/main.rs
@@ -1,29 +1,17 @@
-extern crate sdl2;
 
-use std::rc::Rc;
+
 use std::sync::Arc;
-
-use anyhow::Result;
-use image::EncodableLayout;
-use material::{Dielectric, Lambertian, Material, Metal};
-use math::RectSize;
-use ray::Hittable;
-use render::{Drawable, SdlWindow, SdlRenderer};
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use world::Camera;
-
-use crate::geom::Sphere;
-use crate::{ray::HitList, vec::Vec3};
-
-mod geom;
-mod material;
-mod math;
-mod ppm;
-mod ray;
-mod render;
-mod vec;
-mod world;
+use eframe::epaint::ColorImage;
+use image::ImageBuffer;
+use image::Rgb;
+use image::Rgba;
+use log::*;
+use poll_promise::Promise;
+use rad::geom::Sphere;
+use rad::material::{Lambertian, Metal, Dielectric};
+use rad::math::RectSize;
+use rad::ray::HitList;
+use rad::render::RayRenderer;
 
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const IMAGE_WIDTH: u32 = 800;
@@ -33,8 +21,11 @@ const MAX_SCATTER_DEPTH: u32 = 50;
 // TODO :: Overall Cleanup
 
 use eframe::egui;
+use rad::vec::Vec3;
+
 fn main() -> anyhow::Result<()> {
     env_logger::init();
+    
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(IMAGE_WIDTH as f32, IMAGE_HEIGHT as f32)),
         ..Default::default()
@@ -42,126 +33,229 @@ fn main() -> anyhow::Result<()> {
     let _ = eframe::run_native(
         "Raydium",
         options,
-        Box::new(|_cc| Box::<Raydium>::default()),
+        Box::new(|cc| {
+            let app = Raydium::new(cc, RectSize { width: IMAGE_WIDTH, height: IMAGE_HEIGHT });
+            Box::new(app)
+        }),
     );
+    
     Ok(())
 }
 
-struct Raydium;
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum RenderState {
+    Ready, Running, Finished, Progress(f32), RequestDraw,
+}
 
-impl Default for Raydium {
+impl Default for RenderState {
     fn default() -> Self {
-        Raydium
+        Self::Ready
+    }
+}
+
+#[derive(Clone)]
+struct RaytraceFrame(ImageBuffer<Rgba<u8>, Vec<u8>>);
+
+unsafe impl Send for RaytraceFrame {}
+unsafe impl Sync for RaytraceFrame {}
+
+struct RayRendererAsync {
+    this: RayRenderer,
+    world: Arc<HitList<Sphere>>,
+    surface_size: RectSize,
+}
+
+impl RayRendererAsync {
+    // fn draw_frame(&mut self) -> Result<RaytraceFrame, Box<dyn std::error::Error>> {
+    //     {
+    //         let mut s = self.render_state.write()?;
+    //         *s = RenderState::Running;
+    //     }
+
+    //     let image = self.render_world_to_image();
+        
+    //     {
+    //         let mut s = self.render_state.write()?;
+    //         *s = RenderState::Finished;
+    //     }
+        
+    //     Ok(image.clone())
+    // }
+
+    #[inline]
+    fn render_world_to_image(&self) -> RaytraceFrame {
+        RaytraceFrame(
+            self.this.render_world_to_image(self.world.as_ref(), self.surface_size, MAX_SCATTER_DEPTH)
+        )
+    }
+
+}
+
+
+
+// impl Handler<DrawWorld> for RayRendererAsync {
+//     type Result = ();
+
+//     fn handle(&mut self, msg: DrawWorld, ctx: &mut Self::Context) -> Self::Result {
+
+
+
+//     }
+// }
+
+
+struct Raydium {
+    renderer: Arc<RayRendererAsync>,
+    render_state: RenderState,
+    display_texture: Option<egui::TextureHandle>,
+    render_rx: Option<Promise<RaytraceFrame>>,
+}
+
+// unsafe impl Send for RayRendererAsync {}
+// unsafe impl Sync for RayRendererAsync {}
+
+impl Raydium { 
+    pub fn new(_cc: &eframe::CreationContext<'_>, surface_size: RectSize) -> Self {
+
+        const BEGIN_STATE: RenderState = RenderState::Ready;
+
+        let world = Self::create_world();
+        let render_state = BEGIN_STATE;
+        let renderer = Arc::new(RayRendererAsync {
+            this: RayRenderer::default(),
+            world: world.clone(),
+            surface_size,
+        });
+        
+        //renderer.do_send(DrawWorld);
+        
+        Self {
+            renderer,
+            render_state,
+            display_texture: None,
+            render_rx: None
+        }
+    }
+
+    fn display_panel(&mut self, ctx: &egui::Context) {
+        egui::SidePanel::left("Render Panel").show(ctx, |ui| { 
+            let state_text = {
+                let st = match self.render_state {
+                    RenderState::Ready => "Ready".into(),
+                    RenderState::Running => "Running".into(),
+                    RenderState::Finished => "Finished".into(),
+                    RenderState::Progress(percent) => format!("{}%", percent * 100.0),
+                    RenderState::RequestDraw => "Requesting Draw".into(),
+                };
+                st
+            };
+
+            ui.label(format!("Render State: {}", state_text));
+            if ui.button("Render Frame").clicked() {
+                let rs = self.render_state;
+                if rs == RenderState::Ready || rs == RenderState::Finished {
+                    self.render_state = RenderState::Running;
+                    let renderer = self.renderer.clone();
+                    let receiver = Promise::spawn_thread("Raydium Render", move || {
+                        renderer.render_world_to_image()
+                    });         
+                    self.render_rx = Some(receiver);
+                }     
+            }
+        });
+    }
+
+
+    fn create_world() -> Arc<HitList<Sphere>> {
+        
+        let mut world = HitList::new();
+
+        let mat_ground = Arc::new(Lambertian::new(Vec3(0.8, 0.8, 0.0)));
+        let mat_center = Arc::new(Lambertian::new(Vec3(0.1, 0.2, 0.5)));
+        let mat_left = Arc::new(Dielectric::new(1.5));
+        let mat_right = Arc::new(Metal::new(Vec3(0.8, 0.6, 0.2), 0.0));
+
+        world.0.push(Arc::new(Sphere::new(
+            mat_ground,
+            Vec3(0.0, -100.5, -1.0),
+            100.0,
+        )));
+        world.0.push(Arc::new(Sphere::new(
+            mat_center.clone(),
+            Vec3(0.0, 0.0, -1.0),
+            0.5,
+        )));
+        world.0.push(Arc::new(Sphere::new(
+            mat_left.clone(),
+            Vec3(-1.0, 0.0, -1.0),
+            0.5,
+        )));
+        world.0.push(Arc::new(Sphere::new(
+            mat_left.clone(),
+            Vec3(-1.0, 0.0, -1.0),
+            -0.4,
+        )));
+        world.0.push(Arc::new(Sphere::new(
+            mat_right.clone(),
+            Vec3(1.0, 0.0, -1.0),
+            0.5,
+        )));
+        Arc::new(world)
     }
 }
 
 impl eframe::App for Raydium {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
+        if let Some(ref prom) = self.render_rx {
+            if let Some(image) = prom.ready() {
+                
+                let pixels = image.0.as_flat_samples();
+                let size = [image.0.width() as _, image.0.height() as _];
+                let texture_image = ColorImage::from_rgba_unmultiplied(size, pixels.as_slice());
+
+                self.display_texture = Some(                    
+                    ctx.load_texture("Raycast Image", texture_image, Default::default())
+                );
+                self.render_state = RenderState::Ready;
+                self.render_rx = None;
+            }
+        } 
+
+        self.display_panel(ctx);
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Raydium test gui");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Ayyyeee lmao");
-            });
-            if ui.button("Click me").clicked() {
-                ui.label("Clicked!!!");
+            if let Some(ref texture) = self.display_texture {
+                ui.image(texture, texture.size_vec2());
+            } else {
+                ui.label("No Image");
             }
         });
     }
 }
 
-fn _main() -> Result<(), Box<dyn std::error::Error>> {
-    let window = SdlWindow::build_new(IMAGE_WIDTH, IMAGE_HEIGHT, "Raydium")?;
-    let cam = Camera::new(
-        Vec3(-2.0, 2.0, 1.0),
-        Vec3(0.0, 0.0, -1.0),
-        Vec3(0.0, 1.0, 0.0),
-        20.0,
-        ASPECT_RATIO,
-    );
-    let renderer = SdlRenderer::new(window, cam);
 
-    /**********************************************
-     *
-     */
 
-    let mut world = HitList::new();
+// impl actix::Supervised for Raydium {
+//     fn restarting(&mut self, ctx: &mut <Self as Actor>::Context) {}
+// }
 
-    let mat_ground = Arc::new(Lambertian::new(Vec3(0.8, 0.8, 0.0)));
-    let mat_center = Arc::new(Lambertian::new(Vec3(0.1, 0.2, 0.5)));
-    let mat_left = Arc::new(Dielectric::new(1.5));
-    let mat_right = Arc::new(Metal::new(Vec3(0.8, 0.6, 0.2), 0.0));
+// fn _main() -> Result<(), Box<dyn std::error::Error>> {
+//     env_logger::init();
 
-    world.0.push(Arc::new(Sphere::new(
-        mat_ground,
-        Vec3(0.0, -100.5, -1.0),
-        100.0,
-    )));
-    world.0.push(Arc::new(Sphere::new(
-        mat_center.clone(),
-        Vec3(0.0, 0.0, -1.0),
-        0.5,
-    )));
-    world.0.push(Arc::new(Sphere::new(
-        mat_left.clone(),
-        Vec3(-1.0, 0.0, -1.0),
-        0.5,
-    )));
-    world.0.push(Arc::new(Sphere::new(
-        mat_left.clone(),
-        Vec3(-1.0, 0.0, -1.0),
-        -0.4,
-    )));
-    world.0.push(Arc::new(Sphere::new(
-        mat_right.clone(),
-        Vec3(1.0, 0.0, -1.0),
-        0.5,
-    )));
-    // world
-    //     .0
-    //     .push(Box::new(Sphere::new(Vec3(0.0, 0.0, -1.0), 0.5)));
-    // world
-    //     .0
-    //     .push(Box::new(Sphere::new(Vec3(0.0, -100.5, -1.0), 100.0)));
+//     /**********************************************
+//      *
+//      */
 
-    let image = renderer.render_world_to_image(
-        &world,
-        RectSize {
-            width: IMAGE_WIDTH,
-            height: IMAGE_HEIGHT,
-        },
-        MAX_SCATTER_DEPTH,
-    )?;
-    // image = image.flipv();
-    // image
-    //     .save("_image.ppm")
-    //     .expect("Failed to save image");r
-    image.save("_image.ppm")?;
+//     // world
+//     //     .0
+//     //     .push(Box::new(Sphere::new(Vec3(0.0, 0.0, -1.0), 0.5)));
+//     // world
+//     //     .0
+//     //     .push(Box::new(Sphere::new(Vec3(0.0, -100.5, -1.0), 100.0)));
 
-    /***********************************/
+//     let image = renderer.render_world_to_image(
+//     Ok(())
 
-    let mut tex = renderer.create_static_texture(IMAGE_WIDTH, IMAGE_HEIGHT)?;
-    let _ = tex.copy(image.as_bytes(), (IMAGE_WIDTH * 3) as usize)?;
-
-    let mut epump = renderer.radwindow().events_mut();
-    'running: loop {
-        for event in epump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
-                    break 'running;
-                }
-                _ => {}
-            }
-        }
-
-        renderer.clear_black();
-
-        renderer.draw(&tex);
-
-        renderer.swap_buffers();
-    }
-    Ok(())
-}
+//     /***********************************/
+// }
--- raydium/src/render.rs
+++ raydium/src/render.rs
@@ -5,181 +5,86 @@ use std::{
     time::Instant,
 };
 
-use image::{DynamicImage, ImageBuffer, Rgb, RgbImage};
+use image::{DynamicImage, ImageBuffer, Rgb, RgbImage, Rgba};
 use rand::{Rng, SeedableRng};
 use rayon::prelude::{IntoParallelIterator, ParallelBridge, ParallelIterator};
-use sdl2::{
-    event::Event,
-    keyboard::Keycode,
-    pixels::{Color, PixelFormatEnum},
-    rect::Rect,
-    render::{Canvas, Texture, TextureCreator},
-    sys::SDL_Texture,
-    video::Window,
-    Sdl, VideoSubsystem,
-};
 
 use crate::{
-    geom::SdlTexture,
     math::{clamp, IOResult, RectSize},
     ray::{HitList, Hittable},
     vec::Vec3,
     world::Camera,
 };
 
-pub struct SdlWindow {
-    ctx: Sdl,
-    video: VideoSubsystem,
-    canvas: RefCell<Canvas<sdl2::video::Window>>,
-    event_pump: RefCell<sdl2::EventPump>,
+pub mod defaults {
+    pub const NUM_SAMPLES: u32 = 100;
+    pub const ASPECT_RATIO: f64 = 16.0 / 9.0;
 }
 
-pub struct SdlRenderer {
-    window: SdlWindow,
+#[derive(Clone, Debug)]
+pub struct RayRenderer {
     camera: Camera,
-    texture_creator: Rc<TextureCreator<sdl2::video::WindowContext>>,
+    num_samples: u32,
 }
 
-impl SdlWindow {
-    pub fn build_new(
-        width: u32,
-        height: u32,
-        title: &str,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
-        let sdl = sdl2::init()?;
-        let video = sdl.video()?;
-        let win = video
-            .window(title, width, height)
-            .vulkan()
-            .resizable()
-            .build()?;
-
-        let mut canvas = win.into_canvas().build()?;
-
-        canvas.set_draw_color(Color::BLACK);
-        canvas.clear();
-        canvas.present();
-        Ok(SdlWindow {
-            ctx: sdl.clone(),
-            video,
-            canvas: RefCell::new(canvas),
-            event_pump: RefCell::new(sdl.event_pump()?),
-        })
-    }
-
-    pub fn events(&self) -> Ref<sdl2::EventPump> {
-        self.event_pump.borrow()
-    }
-    pub fn events_mut(&self) -> RefMut<sdl2::EventPump> {
-        self.event_pump.borrow_mut()
-    }
-}
-
-impl SdlRenderer {
-    const DEFAULT_NUM_SAMPLES: u32 = 100;
-
-    pub fn new(window: SdlWindow, camera: Camera) -> Self {
-        let c = {
-            let c = window.canvas.borrow();
-            let tc = Rc::new(c.texture_creator());
-            tc
-        };
-
+impl Default for RayRenderer {
+    fn default() -> Self {
         Self {
-            window,
-            camera,
-            texture_creator: c,
+            camera: Camera::new(
+                Vec3(-2.0, 2.0, 1.0),
+                Vec3(0.0, 0.0, -1.0),
+                Vec3(0.0, 1.0, 0.0),
+                90.0,
+                defaults::ASPECT_RATIO,
+            ),
+            num_samples: defaults::NUM_SAMPLES
         }
-    }
-
-    pub const fn radwindow(&self) -> &SdlWindow {
-        &self.window
-    }
-    pub fn canvas(&self) -> Ref<Canvas<sdl2::video::Window>> {
-        self.window.canvas.borrow()
-    }
-    pub fn canvas_mut(&self) -> RefMut<Canvas<sdl2::video::Window>> {
-        self.window.canvas.borrow_mut()
-    }
-    pub const fn window(&self) -> &SdlWindow {
-        &self.window
-    }
-
-    pub fn create_static_texture(&self, width: u32, height: u32) -> IOResult<SdlTexture> {
-        let t =
-            self.texture_creator
-                .create_texture_static(PixelFormatEnum::RGB24, width, height)?;
-        let st = SdlTexture::new(t, Rect::new(0, 0, width, height));
-        Ok(st)
-    }
-
-    pub fn create_streaming_texture(&self, width: u32, height: u32) -> IOResult<SdlTexture> {
-        let t =
-            self.texture_creator
-                .create_texture_streaming(PixelFormatEnum::RGB24, width, height)?;
-        let st = SdlTexture::new(t, Rect::new(0, 0, width, height));
-        Ok(st)
-    }
-
-    pub fn clear_black(&self) {
-        let mut c = self.canvas_mut();
-        c.set_draw_color(Color::BLACK);
-        c.clear();
-    }
-
-    pub fn draw_texture(&self, tex: &Texture, tex_rect: &Rect) {
-        let mut c = self.canvas_mut();
-        let _ = c.copy(tex, None, Some(*tex_rect));
-    }
-
-    pub fn swap_buffers(&self) {
-        let mut c = self.canvas_mut();
-        c.present();
-    }
-
-    // TODO :: This shit slow. Make it concurrent/multithreaded
-    // TODO :: Put this in World with the Drawable trait
-    pub fn render_world_to_image<T: Hittable + Send + Sync>(
-        &self,
-        world: &HitList<T>,
-        size: RectSize,
-        scatter_depth: u32,
-    ) -> IOResult<ImageBuffer<Rgb<u8>, Vec<u8>>> {
-        let RectSize { width, height } = size;
-
-        let mut image = DynamicImage::new_rgb8(width, height);
-        let buffer = image
-            .as_mut_rgb8()
-            .expect("Failed to convert image to rgb8");
-
-        {
-            let start = Instant::now();
-            println!("Start render");
-            draw_frame_parallel(
-                buffer,
-                &self.camera,
-                &world,
-                Self::DEFAULT_NUM_SAMPLES,
-                scatter_depth,
-                size,
-            );
-            println!("End render: Elapsed: {:.2?}", start.elapsed());
-        }
-
-        if let Some(image) = image.flipv().as_rgb8() {
-            Ok(image.to_owned())
-        } else {
-            Err("Failed to convert image to rgb8".into())
-        }
-    }
-
-    pub fn draw<T: Drawable>(&self, drawable: &T) {
-        drawable.draw(self);
     }
 }
+
+impl RayRenderer {
+
+        pub const fn new(camera: Camera, num_samples: u32) -> Self {
+            Self { camera, num_samples }
+        }
+
+        // TODO :: Put this in World with the Drawable trait
+        pub fn render_world_to_image<T: Hittable + Send + Sync>(
+            &self,
+            world: &HitList<T>,
+            size: RectSize,
+            scatter_depth: u32,
+        ) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+            let RectSize { width, height } = size;
+    
+            let mut image = DynamicImage::new_rgba8(width, height);
+            let buffer = image
+                .as_mut_rgba8()
+                .expect("Failed to convert image to rgb8");
+    
+            {
+                let start = Instant::now();
+                println!("Start render");
+                draw_frame_parallel(
+                    buffer,
+                    &self.camera,
+                    &world,
+                    defaults::NUM_SAMPLES,
+                    scatter_depth,
+                    size,
+                );
+                println!("End render: Elapsed: {:.2?}", start.elapsed());
+            }
+            
+
+           image.flipv().as_rgba8().expect("Failed to convert image to rgb8").clone()
+        }
+}
+
+
 
 fn draw_frame_parallel<T: Hittable + Sync + Send>(
-    buffer: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
+    buffer: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
     camera: &Camera,
     world: &HitList<T>,
     num_samples: u32,
@@ -210,7 +115,7 @@ fn draw_frame_parallel<T: Hittable + Sync + Send>(
         });
 }
 
-fn write_color(pixel: &mut Rgb<u8>, color: &Vec3, samples_per_pixel: u32) {
+fn write_color(pixel: &mut Rgba<u8>, color: &Vec3, samples_per_pixel: u32) {
     let scale = 1.0 / samples_per_pixel as f64;
     let r = (color.x() * scale).sqrt();
     let g = (color.y() * scale).sqrt();
@@ -218,8 +123,5 @@ fn write_color(pixel: &mut Rgb<u8>, color: &Vec3, samples_per_pixel: u32) {
     pixel[0] = (255.0 * clamp(r, 0.0, 0.999)) as u8;
     pixel[1] = (255.0 * clamp(g, 0.0, 0.999)) as u8;
     pixel[2] = (255.0 * clamp(b, 0.0, 0.999)) as u8;
-}
-
-pub trait Drawable {
-    fn draw(&self, renderer: &SdlRenderer);
+    pixel[3] = 255;
 }
